c50b7e3ecc5ce1b3eb5f5e9d28227dbdd7d0e37c
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-05-28 22:26:50 +0530

    whatsapp automation

---
+++ WhatsappAutomation/whatsapp.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+driver= webdriver.Chrome()
+driver.get('https://web.whatsapp.com')
+
+contact=input('enter name of your contact: ')
+script=input('enter the script to spam: ')
+msg=list(script.split())
+# input("if QR code is scanned then press enter, otherwise scan QR code")
+user=driver.find_element_by_xpath('//span[@title="{}"]'.format(contact))
+user.click()
+msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div' )
+for i in msg:
+    msg_box.send_keys(i)
+    button=driver.find_element_by_class_name('_35EW6')
+    button.click()
+print('Done')
