e81225ccabe47f19ee9e7d9c4ee181617d17c362
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-08-09 01:00:10 +0530

    update

--- WhatsappAutomation/whatsapp.py
+++ WhatsappAutomation/whatsapp.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
+
+# driver= webdriver.Chrome()
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.get('https://web.whatsapp.com')
 driver= webdriver.Chrome()
 driver.get('https://web.whatsapp.com')
 
@@ -12,6 +18,7 @@ user.click()
 msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div' )
 for i in msg:
     msg_box.send_keys(i)
-    button=driver.find_element_by_class_name('_35EW6')
-    button.click()
+    # button=driver.find_element_by_class_name('_35EW6')
+    # button.click()
+    msg_box.send_keys(Keys.ENTER)
 print('Done')
