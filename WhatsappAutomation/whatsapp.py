d60540811e53eb0e59c3c9bf5a863c77ee9a700f
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-08-13 01:25:53 +0530

    cleanup

--- WhatsappAutomation/whatsapp.py
+++ WhatsappAutomation/whatsapp.py
@@ -3,10 +3,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
 
-# driver= webdriver.Chrome()
+# driver= webdriver.Chrome() #comment in this line if using windows
 driver = webdriver.Chrome(ChromeDriverManager().install())  # comment out this line if useing windows
 driver.get('https://web.whatsapp.com')  # comment out this line if useing windows
-# driver= webdriver.Chrome() #comment in this line if using windows
+
 
 contact=input('enter name of your contact: ')
 script=input('enter the script to spam: ')
@@ -17,7 +17,5 @@ user.click()
 msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div' )
 for i in msg:
     msg_box.send_keys(i)
-    # button=driver.find_element_by_class_name('_35EW6')
-    # button.click()
     msg_box.send_keys(Keys.ENTER)
 print('Done')
