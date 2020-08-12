03f0e39fa340c059f3db18d4eb7fadd3c6de66da
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2020-08-13 01:22:28 +0530

    fix for ubuntu

--- WhatsappAutomation/whatsapp.py
+++ WhatsappAutomation/whatsapp.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
 
 # driver= webdriver.Chrome()
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.get('https://web.whatsapp.com')
-driver= webdriver.Chrome()
-driver.get('https://web.whatsapp.com')
+driver = webdriver.Chrome(ChromeDriverManager().install())  # comment out this line if useing windows
+driver.get('https://web.whatsapp.com')  # comment out this line if useing windows
+# driver= webdriver.Chrome() #comment in this line if using windows
 
 contact=input('enter name of your contact: ')
 script=input('enter the script to spam: ')
 msg=list(script.split())
-# input("if QR code is scanned then press enter, otherwise scan QR code")
+input("if QR code is scanned then press enter, otherwise scan QR code")
 user=driver.find_element_by_xpath('//span[@title="{}"]'.format(contact))
 user.click()
 msg_box = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div' )
